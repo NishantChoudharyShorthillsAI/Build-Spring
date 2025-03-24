4045368fce99bd7a44b88f91aa9075fac8877288
NishantChoudharyShorthillsAI <nishant.choudhary@shorthills.ai>
2025-03-24 21:07:19 +0000

    frontend change

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello World! I am Nishant Choudary";
+        return "Hello World! I am Nishant Choudary and I am in DevOps Team";
     }
 }
 
