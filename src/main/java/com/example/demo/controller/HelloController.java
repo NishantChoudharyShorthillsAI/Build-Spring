2a4e2228a8a7dce95a19cf5ad4c7b84da1051d91
NishantChoudharyShorthillsAI <nishant.choudhary@shorthills.ai>
2025-03-25 10:10:53 +0000

    text add

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hey World! I am Nishant Choudary and I am in DevOps Team I am in my Office. And i am very happy to be here.Hey Devops Team";
+        return "Hey World! I am Nishant Choudary and I am in DevOps Team I am in my Office. And i am very happy to be here.Hey Devops Team abcdef";
     }
 }
 
